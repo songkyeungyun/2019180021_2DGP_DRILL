96b6bd17a35c05a13b8e5371a3085ae309a07b63
songkyeungyun <113000022+songkyeungyun@users.noreply.github.com>
2022-09-18 21:37:25 +0900

    DRILL 05
    2019180021 송경윤

---
+++ 과제 폴더 1/drill.5.py
@@ -0,0 +1,34 @@
+import turtle
+
+def restart():
+    turtle.reset()
+
+
+def move_w():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_a():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_s():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def move_d():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+    
+turtle.shape('turtle')
+turtle.onkey(move_w,'w')
+turtle.onkey(move_a,'a')
+turtle.onkey(move_s,'s')
+turtle.onkey(move_d,'d')
+turtle.onkey(restart,'Escape')
+turtle.listen()

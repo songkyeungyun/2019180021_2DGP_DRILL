ce3995ec988f66d8c81a83cab549a2e14a238c29
songkyeungyun <113000022+songkyeungyun@users.noreply.github.com>
2022-11-24 12:54:57 +0900

    drill 14
    2019180021 송경윤

---
+++ project 9/dirll 14/ball.py
@@ -0,0 +1,28 @@
+from pico2d import *
+import random
+import game_world
+import server
+
+class Ball:
+    image = None
+
+    def __init__(self):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x = random.randint(0, 1800)
+        self.y = random.randint(0, 1000)
+
+    def draw(self):
+        sx, sy = self.x - server.background.window_left, self.y - server.background.window_bottom
+        self.image.draw(sx, sy)
+
+    def update(self):
+        pass
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self, other, group):
+        print('ball disappear')
+        if group == 'boy:ball':
+            game_world.remove_object(self)
